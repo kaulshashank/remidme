b06bafaaa7e4063f93ddf97815127ee0184e0bc7
Hrittik Bhattaharjee <impulsiv.rekt.mind@gmail.com>
2019-09-23 02:13:28 +0530

    refactored and added parser module

--- app.js
+++ app.js
@@ -1,49 +1,5 @@
-function test(taskArr) {
-    if(taskArr[2] == 'to' && (taskArr[4] == 'in' || taskArr[4] == 'after')) {
-        let task =  taskArr[3];
-        let time =  taskArr[5];
-        let timeUnit = taskArr[6];
+const parser = require("./parse.js");
 
-        switch (timeUnit) {
-            case 'hours':
-                callTimeout(task, time*3600000);
-                break;
-            case 'minutes':
-                callTimeout(task, time*60000);
-                break;
-            case 'seconds':
-                callTimeout(task, time*1000);
-                break;
-        }
-    }
-    if(taskArr[2] == 'to' && (taskArr[4] == 'in' || taskArr[4] == 'after') && taskArr[5] == 'every') {
-        let task =  taskArr[3];
-        let time =  taskArr[6];
-        let timeUnit = taskArr[7];
+const something = parser(process.argv);
 
-        switch (timeUnit) {
-            case 'hours':
-                callInterval(task, time*3600000);
-                break;
-            case 'minutes':
-                callInterval(task, time*60000);
-                break;
-            case 'seconds':
-                callInterval(task, time*1000);
-                break;
-        }
-    }
-    else {
-        console.log("error occured");
-    }
-}
-
-function callTimeout(task, time) {
-    setTimeout(() => console.log(task), time);
-}
-
-function callInterval(task, time) {
-    setInterval(() => console.log(task), time);
-}
-
-test(process.argv);
+console.log(something);
